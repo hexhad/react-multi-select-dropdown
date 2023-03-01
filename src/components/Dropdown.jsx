793a293d5eca6f64695220ab932625b486470783
hexhad <hexhad@gmail.com>
2023-03-02 01:36:54 +0530

    fin

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,12 +4,15 @@ import { resposnse } from "../api/uss";
 
 export default function Dropdown() {
   const [isVisible, setIsVisible] = useState(false);
+  const [apiResp, setApiResp] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
   const [selectedItems, setSelectedItems] = useState(
-    resposnse.reduce(
-      (obj, item) => ({ ...obj, [item.abbreviation]: false }),
-      {}
-    )
+    apiResp.reduce((obj, item) => ({ ...obj, [item.itm_id]: false }), {})
   );
+
+  useEffect(() => {
+    setApiResp(resposnse);
+  }, []);
 
   const dropdownRef = useRef(null);
   const buttonRef = useRef(null);
@@ -30,38 +33,28 @@ export default function Dropdown() {
   const numberOfSelected = Object.values(selectedItems).filter(Boolean).length;
 
   const filterContent = (e) => {
-    console.log(e.target.value);
+    setSearchParam(e.target.value);
+    setApiResp(
+      resposnse.filter((itm) =>
+        itm.name.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
   };
 
   return (
     <>
       <button
-      className="dropdown-button"
-      ref={buttonRef}
+        className="dropdown-button"
+        ref={buttonRef}
         onClick={(e) => {
           e.stopPropagation();
-          const { offsetLeft , offsetTop,offsetWidth } = buttonRef.current
-          console.log(offsetLeft, offsetTop,offsetWidth);
+          const { offsetLeft, offsetTop, offsetWidth } = buttonRef.current;
+          console.log(offsetLeft, offsetTop, offsetWidth);
           setIsVisible((prev) => !prev);
         }}
       >
-        {!!numberOfSelected ? `selected ${numberOfSelected}` : "dropdown"}
+        {!!numberOfSelected ? `selected ${numberOfSelected}` : "Click Me !"}
         {isVisible ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        ) : (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -76,6 +69,21 @@ export default function Dropdown() {
               d="M4.5 15.75l7.5-7.5 7.5 7.5"
             />
           </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
         )}
       </button>
       {isVisible && (
@@ -84,26 +92,36 @@ export default function Dropdown() {
           onClick={(e) => e.stopPropagation()}
           className="panel"
         >
-          <input type={"text"} className="dropdown-search" onChange={filterContent} />
-          {resposnse.map((item) => (
-            <fieldset
-              key={item.abbreviation}
-              className={`dropdown-item ${selectedItems[item.abbreviation] ? "selected" : ""} `}
-            >
-              <input
-                onChange={(e) =>
-                  setSelectedItems({
-                    ...selectedItems,
-                    [item.abbreviation]: e.target.checked,
-                  })
-                }
-                checked={selectedItems[item.abbreviation]}
-                id={`input-${item.abbreviation}`}
-                type={"checkbox"}
-              />
-              <label htmlFor={`input-${item.abbreviation}`}>{item.name}</label>
-            </fieldset>
-          ))}
+          <input
+            placeholder="Search"
+            value={searchParam}
+            type={"text"}
+            className="dropdown-search"
+            onChange={filterContent}
+          />
+          <div className="selections">
+            {apiResp.map((item) => (
+              <fieldset
+                key={item.itm_id}
+                className={`dropdown-item ${
+                  selectedItems[item.itm_id] ? "selected" : ""
+                } `}
+              >
+                <input
+                  onChange={(e) =>
+                    setSelectedItems({
+                      ...selectedItems,
+                      [item.itm_id]: e.target.checked,
+                    })
+                  }
+                  checked={selectedItems[item.itm_id]}
+                  id={`input-${item.itm_id}`}
+                  type={"checkbox"}
+                />
+                <label htmlFor={`input-${item.itm_id}`}>{item.name}</label>
+              </fieldset>
+            ))}
+          </div>
         </div>
       )}
     </>
